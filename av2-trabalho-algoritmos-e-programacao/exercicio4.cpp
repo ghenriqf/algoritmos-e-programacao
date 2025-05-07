0f670c9ec514dbd0a5d7774972f94adc835c9d00
Gabriel Henrique <gabrielhenriquefreitas28@gmail.com>
2025-05-07 13:30:29 -0300

    Atualiza resolução do exercício 4

--- av2-trabalho-algoritmos-e-programacao/exercicio4.cpp
+++ av2-trabalho-algoritmos-e-programacao/exercicio4.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main(){
+    int n, soma = 0;
+
+    cout << "Digite um valor x: ";
+    cin >> n;
+
+    while(n>0){
+        soma += n % 10;
+        n = n / 10;        
+    }
+    
+    cout << "A soma dos valores que compoem X e de " << soma;
 
     return 0;
 }
