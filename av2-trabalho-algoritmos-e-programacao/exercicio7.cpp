ca437bf54123965bf57e167fd028e5d8311b58fb
Gabriel Henrique <gabrielhenriquefreitas28@gmail.com>
2025-05-09 00:28:23 -0300

    Atualiza a resolução do exercício 7

--- av2-trabalho-algoritmos-e-programacao/exercicio7.cpp
+++ av2-trabalho-algoritmos-e-programacao/exercicio7.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(){
+    
+    int n;
+    cout << "Forneca n termos para a sequencoa fobonacci: ";
+    cin >> n;
+
+    int f1=1,f2=2,f3;
+
+    cout << 1 << " " << f1 << " " << f2;
+    for(int i = 0; i<=n; i++){
+        f3 = f1 + f2;
+        cout << " " << f3;
+        f1 = f2;
+        f2 = f3;
+        
+    }
 
     return 0;
 }
