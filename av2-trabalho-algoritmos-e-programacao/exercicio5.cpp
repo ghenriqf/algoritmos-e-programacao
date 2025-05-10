24d51d2929ac39d9e6ef3068980a7f3a641dcf85
Gabriel Henrique <gabrielhenriquefreitas28@gmail.com>
2025-05-10 17:30:14 -0300

    Atualiza a resolução do exercício 5

--- av2-trabalho-algoritmos-e-programacao/exercicio5.cpp
+++ av2-trabalho-algoritmos-e-programacao/exercicio5.cpp
@@ -3,5 +3,32 @@ using namespace std;
 
 int main(){
 
-    return 0;
+    int a,b, soma = 0;
+    
+    cout << "Digite os valores para um intervalo [a,b]: " << endl;
+    cin >> a >> b; 
+
+    if(a<= 1 or b<=1){
+        cout << "Um numero primo e um numero inteiro maior que 1";
+        return 1;
+    }
+
+    for(int min = a; min<=b; min++){
+        if(min == 2){
+            soma+=min;
+        }
+        else{
+            for(int i = 2; i<= min-1; i++){
+                if(min%i == 0){
+                    i = min-1;
+                }else{
+                    if(i == min-1){
+                        soma+=min;
+                    }
+                }
+        
+            }
+        }
+    }
+    cout << "A soma dos numeros primos do intervalo [" << a << "," << b << "]" << " e de " << soma;
 }
