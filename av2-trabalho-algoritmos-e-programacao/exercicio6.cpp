3e6d4bb21360ab271bf444086ea2a8a970ed4e9e
Gabriel Henrique <gabrielhenriquefreitas28@gmail.com>
2025-05-11 20:15:03 -0300

    Atualiza a resolução do exercício 6

--- av2-trabalho-algoritmos-e-programacao/exercicio6.cpp
+++ av2-trabalho-algoritmos-e-programacao/exercicio6.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+    int n,soma=0;
+
+    cout << "Digite um valor n para seber se e um numero perfeito:";
+    cin >> n;
+
+    for(int i = 1; i<n; i++){
+        if(n%i == 0){
+            soma+=i;
+        }
+    }
+
+    if(soma == n){
+        cout << "O numero digitado e perfeito!";
+    }else{
+        cout << "O numero digitado nao e perfeito!";
+    }
+
 
     return 0;
 }
