b103fed7c9a0445232cf5c5915d734d302127d9e
Gabriel Henrique de Freitas <8451672@univali.br>
2025-05-27 20:43:58 -0300

    Adiciona a resolucão do letra A e B do exercicio 1

---
+++ aula-13-algoritmos-e-programacao/exercicio1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+
+int a();
+int b();
+
+int main(){
+
+    a();
+    b();
+    
+
+
+    return 0;
+}
+
+
+int a(){
+
+    int matriz[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    int k = 2;
+
+    cout << "A)" << endl;
+    cout << "Os valores da matriz divisiveis pelo valor k sao: " << endl;
+    cout << "k = " << k << endl;
+
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+           if(matriz[i][j] % k == 0){
+            cout << matriz[i][j] << " ";
+           } 
+        }
+
+    
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+int b(){
+
+    int x[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    int soma = 0;
+
+    cout << "B)" << endl;
+
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            soma = soma + x[i][j];
+        }
+        cout << "Soma linha " << i << " = " << soma << endl;
+        soma = 0;
+        
+    }
+
+    return 0;
+}
