e5e1e50c3de025baeab8dd946335825d7109d62d
Gabriel Henrique de Freitas <8451672@univali.br>
2025-05-27 21:35:42 -0300

    Adiciona a resolução da letra C do exercício 1

--- aula-13-algoritmos-e-programacao/exercicio1.cpp
+++ aula-13-algoritmos-e-programacao/exercicio1.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int a();
 int b();
+int c();
+int d();
 
 int main(){
 
     a();
     b();
+    c();
     
 
 
@@ -53,18 +56,44 @@ int b(){
         {7,8,9}
     };
 
-    int soma = 0;
+    int somaLinha = 0;
 
     cout << "B)" << endl;
 
     for(int i = 0; i<3; i++){
         for(int j = 0; j<3; j++){
-            soma = soma + x[i][j];
+            somaLinha = somaLinha + x[i][j];
         }
-        cout << "Soma linha " << i << " = " << soma << endl;
-        soma = 0;
+        cout << "Soma linha " << i << " = " << somaLinha << endl;
+        somaLinha = 0;
         
     }
+
+    return 0;
+}
+
+int c(){
+
+    int x[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    int soma = 0;
+
+    cout << "C)" << endl;
+
+    for(int j = 0; j<3; j++){
+        soma = 0;
+        for(int i = 0; i<3; i++){
+            soma+= x[i][j];
+        }
+        cout << "A soma da coluna " << j << " e de " << soma << endl;
+          
+    }
+
+    
 
     return 0;
 }
